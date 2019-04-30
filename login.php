57cbc2466dc2976689b0c252edd7dad8b2f8f7b7
alumnat <alumnat@DESKTOP-KS8A00N>
2019-04-30 12:32:57 +0200

    plantiila

--- login.php
+++ login.php
@@ -38,35 +38,35 @@
 					</span>
 
 					<span class="login100-form-title p-b-34 p-t-27">
-						Log in
+						Entra
 					</span>
 
 					<div class="wrap-input100 validate-input" data-validate = "Enter username">
-						<input class="input100" type="text" name="username" placeholder="Username">
+						<input class="input100" type="text" name="username" placeholder="Nom d'usuari">
 						<span class="focus-input100" data-placeholder="&#xf207;"></span>
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="pass" placeholder="Password">
+						<input class="input100" type="password" name="pass" placeholder="Contrasenya">
 						<span class="focus-input100" data-placeholder="&#xf191;"></span>
 					</div>
 
 					<div class="contact100-form-checkbox">
-						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+						<input class="input-checkbox100" id="ckb1" type="checkbox" name="Recordem">
 						<label class="label-checkbox100" for="ckb1">
-							Remember me
+							Recordem
 						</label>
 					</div>
 
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
-							Login
+							Entra
 						</button>
 					</div>
 
 					<div class="text-center p-t-90">
 						<a class="txt1" href="#">
-							Forgot Password?
+							Has oblidat la contrasenya?
 						</a>
 					</div>
 				</form>
